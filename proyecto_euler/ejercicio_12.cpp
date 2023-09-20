f836e42a8c3ccf6c9431dc947b7a1276be761a05
juan victor <vjrm123@users.noreply.github.com>
2023-09-20 16:50:12 -0600

    ultimo ejercicio

---
+++ proyecto_euler/ejercicio_12.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int obtenertriangular(int n) {
+    return (n * (n + 1)) / 2;
+}
+
+int obtenerdivisores(int n) {
+    int cantidad = 0;
+    for (int i = 1; i * i <= n; i++) {
+        if (n % i == 0) {
+            cantidad++;
+            if (i != n / i) {
+                cantidad++;
+            }
+        }
+    }
+    return cantidad;
+}
+
+int encontrar_primer_triangular(int cantidad) {
+    int numero = 1;
+    while (true) {
+        int numero_triangular = obtenertriangular(numero);
+        int cantidad_divisores = obtenerdivisores(numero_triangular);
+        if (cantidad_divisores > cantidad) {
+            return numero_triangular;
+        }
+        numero++;
+    }
+}
